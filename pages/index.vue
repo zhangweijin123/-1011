a9fa2fbe97b43ffddaadaf4e8389e19effbaf766
zhangweijin123 <615745978@qq.com>
2019-10-11 20:06:13 +0800

    轮播图部分完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,58 @@
 <template>
-  <div>
-  
-  </div>
+    <div class="container">
+        <!-- 幻灯片 -->
+        <el-carousel 
+        :interval="5000" 
+        arrow="always">
+            <el-carousel-item 
+            v-for="(item, index) in banners" 
+            :key="index">
+                <div class="banner-image" 
+                :style="`
+                background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `">
+                </div>
+            </el-carousel-item>
+        </el-carousel>
+    </div>
 </template>
 
 <script>
-
-
 export default {
-  
+    data(){
+        return {
+            // 轮播图数据
+            banners: []
+        }
+    },
+
+    mounted(){
+      // 请求轮播图图片数据
+      this.$axios({
+        url:"/scenics/banners",
+      }).then(res=>{
+        const {data} = res.data;
+
+        this.banners = data;
+      })
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
