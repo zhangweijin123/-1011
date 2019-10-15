e607e3a0c00225ed77967443824f992037b5cc18
zhangweijin123 <615745978@qq.com>
2019-10-15 19:30:59 +0800

    特价机票部分完成

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -38,7 +38,17 @@
 
     <!-- 特价机票 -->
     <div class="air-sale">
-        
+        <el-row type="flex" class="air-sale-pic" justify="space-between">
+            <el-col :span="6" v-for="(item, index) in sales" :key="index">
+                <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`">
+                    <img :src="item.cover"/>
+                    <el-row class="layer-bar" type="flex" justify="space-between">
+                        <span>{{item.departCity}}-{{item.destCity}}</span>
+                        <span>￥699</span>
+                    </el-row>
+                </nuxt-link>
+            </el-col>
+        </el-row>
     </div>
   </section>
 </template>
@@ -46,9 +56,21 @@
 <script>
 import SearchForm from "@/components/air/searchForm";
 export default {
+  data(){
+        return {
+            sales: []
+        }
+    },
     components:{
         SearchForm
-    }
+    },
+    mounted(){
+        this.$axios({
+            url: `/airs/sale`
+        }).then(res => {
+            this.sales = res.data.data;
+        });
+    },
 }
 </script>
 
