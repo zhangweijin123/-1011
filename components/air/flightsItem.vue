a7758ec3750de5c37f34e3f997df9832b0edabc5
zhangweijin123 <615745978@qq.com>
2019-10-15 17:25:26 +0800

    计算相隔时间

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -14,7 +14,7 @@
                             <span>{{item.org_airport_name}}</span>
                         </el-col>
                         <el-col :span="8" class="flight-time">
-                            <span>2时20分</span>
+                            <span>{{rankTime}}</span>
                         </el-col>
                         <el-col :span="8" class="flight-airport">
                             <strong>{{item.arr_time}}</strong>
@@ -73,8 +73,31 @@ export default {
             }
         
     },
+    computed:{
+        //计算属性 监听组件内容引用的实例的属性的变化
+        rankTime(){
+            //首先分割数组 取出3两个数值
+            const depTime = this.item.dep_time.split(":") //22,30
+            const arrTime = this.item.arr_time.split(":") //20:00
+            // 第二天 如果出发事件大于到达时间 自动加上24小时处理
+            if(arrTime[0] < depTime[0]){
+                arrTime[0] += 24;
+            }
+            //然后用第一个数值乘以60分钟再加上第二个数值获取总分钟数
+            const start = depTime[0] * 60 + (+depTime[1])
+            const end = arrTime[0] * 60 + (+arrTime[1])
+            //然后再用两个总分钟数相减算出相隔的分钟数
+            const dis = end - start
+            //再用得到的相隔分钟数除以60 在向下取整得出整数的小时数
+            const hours = Math.floor(dis / 60);
+            //在用取余的方法算出剩下的分钟数
+            const min = dis % 60;
+            //最后return出最终相隔的时间结果
+            return `${hours}小时${min}分钟`;
+        }
+    },
     mounted(){
-
+        
     }
 }
 </script>
